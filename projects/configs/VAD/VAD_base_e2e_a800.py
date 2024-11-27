d2cc5e5f3b57d631cf8a4e60aedc68a87cd38c66
Wei Jiang <wei.jiang@inceptio.ai>
2024-11-27 17:59:57 +0800

    increase batch_size and num_works according to A800, but not speed obviously

--- projects/configs/VAD/VAD_base_e2e_a800.py
+++ projects/configs/VAD/VAD_base_e2e_a800.py
@@ -355,8 +355,8 @@ test_pipeline = [
 ]
 
 data = dict(
-    samples_per_gpu=1,
-    workers_per_gpu=4,
+    samples_per_gpu=4,
+    workers_per_gpu=12,
     train=dict(
         type=dataset_type,
         data_root=data_root,
